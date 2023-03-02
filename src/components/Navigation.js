7e8d52f88885f94a8cc0b09272d916de3a719ed1
Firman Ramadhan <firman19ramadhan@gmail.com>
2023-03-02 14:19:10 +0700

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,13 +3,7 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-<<<<<<< HEAD
 import NavbarLogo from "../assets/image/logo-HTA.png";
-=======
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Button from "react-bootstrap/Button";
-import NavbarLogo from "../assets/image/logo-hta.png";
->>>>>>> 530c0a99889ddff3a84c862bdb278421e587f23f
 
 function Navigation() {
   return (
@@ -23,20 +17,12 @@ function Navigation() {
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#services">Services</Nav.Link>
           <Nav.Link href="#contactus">Contact Us</Nav.Link>
-<<<<<<< HEAD
           <Link to="/login-page">
-=======
-          <Link to="/login">
->>>>>>> 530c0a99889ddff3a84c862bdb278421e587f23f
             <button className="w-28 h-9 rounded-full text-l font-medium bg-danger text-white">
               Login
             </button>
           </Link>
-<<<<<<< HEAD
           <Link to="/register-page">
-=======
-          <Link to="/register">
->>>>>>> 530c0a99889ddff3a84c862bdb278421e587f23f
             <button className="ms-3 w-28 h-9 rounded-full text-l font-medium bg-white text-black">
               Register
             </button>
@@ -47,8 +33,4 @@ function Navigation() {
   );
 }
 
-<<<<<<< HEAD
 export default Navigation
-=======
-export default Navigation;
->>>>>>> 530c0a99889ddff3a84c862bdb278421e587f23f

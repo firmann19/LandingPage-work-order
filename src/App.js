cbc28455234647b70d6b3611b52bb3593644af3e
yogaraf <yogarafli853@gmail.com>
2023-03-02 08:53:31 +0700

    Yoga

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import CompleteOrder from './pages/CompleteOrder';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-
+import HomePage from "./pages/HomePage"
 function App() {
   return (
     <>
        <Routes>
+        <Route path="/" element={<HomePage />} />
          <Route path="/login-page" element={<LoginPage />} />
          <Route path="/register-page" element={<RegisterPage />} />
          <Route path="/complete-order" element={<CompleteOrder />} />
---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Footer from "../components/Footer";
+import  Navigation from '../components/Navigation';
+
+const HomePage = () => {
+  return (
+    <>
+    <Navigation />
+    {/* <footer>
+        <Footer />
+    </footer> */}
+    </>
+  )
+}
+
+export default HomePage

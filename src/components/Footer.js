7cb4eb13bfe31347b8466ae55149917540dc64f4
Gigssss <gigss0420@gmail.com>
2023-03-08 08:24:47 +0700

    gigih

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ const Footer = () => {
   return (
     <>
       <div className="footer bg-slate-300">
-        <div className="container-fluid py-4">
+        <div className="container-fluid py-1">
           <div className="container">
             <div className="row">
-              <div className="footer col-lg-3 col-md-4 col-sm-6 mb-4">
+              <div className="footer col-lg-2 col-md-4 col-sm-6 mb-4">
                 <img
                   className="footer-logo"
                   src={BrandLogo}
@@ -17,20 +17,20 @@ const Footer = () => {
                 ></img>
                 <p>PT. HTA Indonesia</p>
               </div>
-              <div className="footer col-lg-5 col-md-4 col-sm-6 mb-4">
-                <h6>Address</h6>
+              <div className="footer col-lg-6 col-md-1">
+                <h6>Alamat</h6>
                 <p>
                   Jl. Millennium Raya Blok H-2 No.2 Kawasan Industri Millennium,
                   Tangerang Regency, Banten 15710
                 </p>
                 <p>021-29006990</p>
               </div>
-              <div className="footer col-lg-4 col-md-4 col-sm-6 mb-2">
+              <div className="footer col-lg-4 col-md-1 mb-4">
                 <h6>Layanan</h6>
-                <p>Troubleshoot PC</p>
-                <p>Troubleshoot Network</p>
-                <p>DataBase & Server</p>
-                <p>Other</p>
+                <p className='f-p mb-2'>- Troubleshoot PC</p>
+                <p className='f-p mb-2'>- Troubleshoot Network</p>
+                <p className='f-p mb-2'>- DataBase & Server</p>
+                <p className='f-p mb-2'>- Other</p>
               </div>
               <hr />
               <p className="footer-p">

afd2d34e990bf3f74b7a30c10e0727505bc3836b
Firman Ramadhan <firman19ramadhan@gmail.com>
2023-05-20 18:04:59 +0700

    update maghrib

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -3,15 +3,16 @@ import {
   USER_LOGOUT,
 } from "./constants";
 
-export function userLogin(token, user, email, departement) {
+export function userLogin(token, user, email, departement, id) {
   return {
     type: USER_LOGIN,
     token,
     user,
     email,
-    departement
+    departement,
+    id
   };
-}
+} //ini 1
 
 export function userLogout() {
   localStorage.removeItem("auth");
